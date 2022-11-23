bcc30e4b5df88c3d19ddbde7f6d38f12d1ff45eb
bitlookt <100030493+bitlookt@users.noreply.github.com>
2022-11-23 18:31:53 +0300

    Update and rename js/modules/tabs.js to js/модули/tabs.js

--- js/модули/tabs.js
+++ js/модули/tabs.js
@@ -1,11 +1,9 @@
 function tabs(tabsSelector, tabsContentSelector, tabsParentSelector, activeClass) {
-    // Tabs
-        
-        let tabs = document.querySelectorAll(tabsSelector),
-        tabsContent = document.querySelectorAll(tabsContentSelector),
-        tabsParent = document.querySelector(tabsContentSelector);
+	let tabs = document.querySelectorAll(tabsSelector),
+		tabsContent = document.querySelectorAll(tabsContentSelector),
+		tabsParent = document.querySelector(tabsParentSelector);
 
-    function hideTabContent() {
+	function hideTabContent() {
         
         tabsContent.forEach(item => {
             item.classList.add('hide');
@@ -15,27 +13,27 @@ function tabs(tabsSelector, tabsContentSelector, tabsParentSelector, activeClass
         tabs.forEach(item => {
             item.classList.remove(activeClass);
         });
-    }
+	}
 
-    function showTabContent(i = 0) {
+	function showTabContent(i = 0) {
         tabsContent[i].classList.add('show', 'fade');
         tabsContent[i].classList.remove('hide');
         tabs[i].classList.add(activeClass);
     }
-
+    
     hideTabContent();
     showTabContent();
 
-    tabsParent.addEventListener('click', function(event) {
+	tabsParent.addEventListener('click', function(event) {
         const target = event.target;
-        if(target && target.classList.contains(tabsSelector.slise(1))) {
+		if(target && target.classList.contains(tabsSelector.slice(1))) {
             tabs.forEach((item, i) => {
                 if (target == item) {
                     hideTabContent();
                     showTabContent(i);
                 }
             });
-        }
+		}
     });
 }
 
